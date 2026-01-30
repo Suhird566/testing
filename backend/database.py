550a12a43098b85654301eb93cce3dc4f86ce772
Suhird566 <suhird.singh@revclerx.com>
2026-01-30 11:56:48 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -6,11 +6,12 @@ DATABASE_URL = "postgresql://postgres:postgres123@localhost:5432/ai_code_review"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-Base = declarative_base()
+Base = declarative_base()GHFHTNFGB
 
-
-
-
+HTFRBG GFXCVH
+GGHFJMNHGFCBV GFHBCV
+VDFSAHJTYJNB 
+GFJKGYTHFBVCGH
 
 
 
